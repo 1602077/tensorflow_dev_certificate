047f3f3b5f07fd8a51d3ee69f422bf7878f6ba07
1602077 <62025739+1602077@users.noreply.github.com>
2021-09-22 22:00:14 +0100

    built inital dense models testing horizon and window sizes

--- milestone_projects/bitPredict/src/dataProccessing.py
+++ milestone_projects/bitPredict/src/dataProccessing.py
@@ -1,16 +1,22 @@
 import os
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 import matplotlib.pyplot as plt
+from pprint import pprint
 
 
-def create_train_test_datasets(filename, log=False):
+def create_train_test_datasets(filename="BTC_USD_2013-10-01_2021-05-18-CoinDesk.csv", test_split=0.2, log=False, window=False, horizon=1, window_size=7):
     """
     Create train and test datasets using a 80:20 split on BTC price data.
 
     params:
-        filename (str)  : filename of csv containing BTC price data to be time forecasted
-        log      (bool) : if true performs basic logging and plotting of train / test sets
+        filename    (str)  : filename of csv containing BTC price data to be time forecasted
+        test_split  (float): size of test set
+        log         (bool) : if true performs basic logging and plotting of train / test sets
+        window      (bool) : if true window data to transform into a supervised learning problem
+        horizon     (int)  : number of days to predict data for
+        window_size (int)  : number of days of data used to predict horizon
     """
 
     df = pd.read_csv(f"../data/{filename}", parse_dates=["Date"], index_col=["Date"])
@@ -31,6 +37,50 @@ def create_train_test_datasets(filename, log=False):
         plot_time_series(X_test, y_test, label="test data")
         plt.savefig("../logs/train_test_set0.png", bbox_inches="tight", dpi=250)
 
+    if window:
+        def labelled_windows(x, horizon=horizon):
+            """
+            Create labels for windows dataset
+            """
+            return x[:, :-horizon], x[:, -horizon:]
+
+
+        def make_windows(x, horizon=horizon, window_size=window_size):
+            """
+            Turns a 1D array into a 2D array of seq labelled windows of window_size with horizon size labels.
+            """
+            # 1. Create a window of specific window_size + horizon
+            window_step = np.expand_dims(np.arange(window_size+horizon), axis=0)
+
+            # 2. Create a 2D array of multiple windows steps (minus 1 to account for 0 index)
+            window_indexes = window_step + np.expand_dims(np.arange(len(x)-(window_size+horizon-1)), axis=0).T
+            # print(f"Window indexes:\n {window_indexes, window_indexes.shape}")
+
+            # 3. Index on the target array (a time series) with 2D array of multiple window steps
+            windowed_array = x[window_indexes]
+
+            # 4. Get labelled windows
+            windows, labels = labelled_windows(windowed_array, horizon=horizon)
+
+            return windows, labels
+
+        def make_train_test_split(windows, labels, test_split=test_split):
+            """
+            Splits matching pairs of windows and labels into train and test sets
+            """
+
+            split_size = int(len(windows) * (1-test_split))
+            train_windows = windows[:split_size]
+            train_labels = labels[:split_size]
+            test_windows = windows[split_size:]
+            test_labels = labels[split_size:]
+            return train_windows, train_labels, test_windows, test_labels
+
+        full_windows, full_labels = make_windows(prices, horizon=horizon, window_size=window_size)
+        train_windows, train_labels, test_windows, test_labels = make_train_test_split(full_windows, full_labels, test_split=test_split)
+
+        return train_windows, train_labels, test_windows, test_labels
+        
 
     return X_train, y_train, X_test, y_test
 
@@ -40,7 +90,7 @@ def plot_time_series(timesteps, values, format=".", start=0, end=None, label=Non
     Plots timesteps against values.
 
     params:
-        timestesp: array of timestep values
+        timesteps: array of timestep values
         values:    array of values across timesteps
         format:    style of plot
         start:     where to start the plot
@@ -56,7 +106,60 @@ def plot_time_series(timesteps, values, format=".", start=0, end=None, label=Non
     return
 
 
+def mean_absolute_scaled_error(y_true, y_pred):
+    """ MASE IMPLIMENTATION (assuming no seasonality of data)"""
+
+    mae = tf.reduce_mean(tf.abs(y_true - y_pred))
+    # find MAE of naive forecast (no seasonality)
+    # our seasonality is 1 day (hence shift of one)
+    mae_naive_no_season = tf.reduce_mean(tf.abs(y_true[1:] - y_true[:-1]))
+
+    return mae / mae_naive_no_season
+
+
+def evaluate_preds(y_true, y_pred):
+    """
+    Calculates various evaluation metrics and returns results as a dictionary
+    """
+
+    # assert float32 datatype for metric calculations
+    y_true = tf.cast(y_true, dtype=tf.float32)
+    y_pred = tf.cast(y_pred, dtype=tf.float32)
+
+    mae = tf.keras.metrics.mean_absolute_error(y_true, y_pred)
+    mse = tf.keras.metrics.mean_squared_error(y_true, y_pred)
+    rmse = tf.sqrt(mse)
+    mape = tf.keras.metrics.mean_absolute_percentage_error(y_true, y_pred)
+    mase = mean_absolute_scaled_error(y_true, y_pred)
+
+    results = {
+        "MAE": mae.numpy(),
+        "MSE": mse.numpy(),
+        "RMSE": rmse.numpy(),
+        "MAPE": mape.numpy(),
+        "MASE": mase.numpy()
+    }
+
+    pprint(results)
+
+    return results
+
+def create_model_checkpoint(model_number, model_name):
+    """
+    Create function to create a model checkpoint of a specific filename
+    """
+    filepath = f"../models/{model_number}/{model_name}"
+    return tf.keras.callbacks.ModelCheckpoint(filepath=filepath, verbose=0, save_best_only=True)
+
+def make_preds(model, input_data):
+    """
+    Uses model to make predictions on input_data.
+    """
+    return tf.squeeze(model.predict(input_data))
+
+
 if __name__ == "__main__":
     filename = "BTC_USD_2013-10-01_2021-05-18-CoinDesk.csv"
-    create_train_test_datasets(filename, log=True)
+    # create_train_test_datasets(filename, log=True)
+    create_train_test_datasets(filename, False, True, 1, 7)
 
---
+++ milestone_projects/bitPredict/src/models.py
@@ -0,0 +1,114 @@
+import sys
+import os
+import tensorflow as tf
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+from pprint import pprint
+
+from dataProccessing import *
+
+MODEL_NAMES = ["0_Baseline_Naive", "1_Dense_W7_H1", "2_Dense_W30_H1", "3_Dense_W30_H7"]
+
+def model_0():
+    """
+    Baseline (Model 0): Naive forecast
+    i.e. predicting the next timestep as the previous timestep
+    """
+
+    X_train, y_train, X_test, y_test = create_train_test_datasets()
+
+    naive_forecast = y_test[:-1]
+
+    plt.figure(figsize=(10,7))
+    plot_time_series(X_test, y_test, start=350, format="-", label="test data")
+    plot_time_series(X_test[1:], naive_forecast, start=350, format="-", label="Naive forecast")
+    plt.savefig("../models/0/forecast.png", bbox_inches="tight", dpi=200)
+
+    model_0_results = evaluate_preds(y_true=y_test[1:], y_pred=naive_forecast)
+
+    return model_0_results
+
+def model_1(dir_num, HORIZON=1, WINDOW_SIZE=7):
+    """
+
+    """
+
+
+    train_windows, train_labels, test_windows, test_labels = create_train_test_datasets(window=True, horizon=HORIZON, window_size=WINDOW_SIZE)
+
+    print(f"Length of train and test sets: {len(train_windows), len(test_windows)}")
+    # Check to see if train labels are the same (before and after window split)
+    X_train, y_train, X_test, y_test = create_train_test_datasets()
+    print(f"Train labels are the same before and after window split: {np.array_equal(np.squeeze(train_labels[:-HORIZON-1]), y_train[WINDOW_SIZE:])}")
+    
+    tf.random.set_seed(42)
+
+    model_1 = tf.keras.Sequential([
+        layers.Dense(128, activation="relu"),
+        layers.Dense(HORIZON, activation="linear")
+    ], name=MODEL_NAMES[1])
+    
+    model_1.compile(loss="mae", optimizer="adam", metrics=["mae", "mse"])
+
+    model_1.fit(x=train_windows,
+                y=train_labels,
+                epochs=100,
+                verbose=2, 
+                batch_size=128, 
+                validation_data=(test_windows, test_labels),
+                callbacks=[create_model_checkpoint(dir_num, MODEL_NAMES[dir_num])]
+    )
+
+    # Load in best performing model
+    model_1 = tf.keras.models.load_model(f"../models/{dir_num}/{MODEL_NAMES[dir_num]}")
+    model_1_preds = make_preds(model_1, test_windows)
+
+    print(f"\nModel {dir_num} Results:\n")
+    model_1_results = evaluate_preds(tf.squeeze(test_labels), model_1_preds)
+
+    # plot model
+    offset = 300
+    plt.figure(figsize=(10,7))
+    # index into test_labels to account for the test window offset
+    plot_time_series(timesteps=X_test[-len(test_windows):],  values=test_labels[:, 0], start=offset, label="test data")
+
+    plot_time_series(timesteps=X_test[-len(test_windows):],  values=model_1_preds, start=offset, format="-", label=f"model {dir_num} preds")
+    plt.savefig(f"../models/{dir_num}/model_{dir_num}_predictions.png", dpi=250, bbox_inches="tight")
+    return
+
+
+def train_all_models():
+    """ Trains all models functionally defined in the format model_{INT} """
+    if not os.path.exists(f"../models/0"):
+        os.mkdir(f"../models/0")
+
+        sys.stdout = open(f"../models/0/{MODEL_NAMES[0]}.log", "w")
+        print(f"Model: {MODEL_NAMES[0]}\n")
+        model_0()
+
+    if not os.path.exists(f"../models/1"):
+        os.mkdir(f"../models/1")
+
+        sys.stdout = open(f"../models/1/{MODEL_NAMES[1]}.log", "w")
+        print(f"Model: {MODEL_NAMES[1]}\n")
+        model_1(1, HORIZON=1, WINDOW_SIZE=7)
+
+    if not os.path.exists(f"../models/2"):
+        os.mkdir(f"../models/2")
+
+        sys.stdout = open(f"../models/2/{MODEL_NAMES[2]}.log", "w")
+        print(f"Model: {MODEL_NAMES[2]}\n")
+        model_1(2, HORIZON=1, WINDOW_SIZE=30)
+
+    if not os.path.exists(f"../models/3"):
+        os.mkdir(f"../models/3")
+
+        sys.stdout = open(f"../models/3/{MODEL_NAMES[3]}.log", "w")
+        print(f"Model: {MODEL_NAMES[3]}\n")
+        model_1(3, HORIZON=7, WINDOW_SIZE=30)
+
+    return
+
+
+if __name__ == "__main__":
+    train_all_models()
