ae838c2c1cb76be625f6977f76a5f08f45a3b717
1602077 <62025739+1602077@users.noreply.github.com>
2021-09-26 09:09:23 +0100

    LSTM model complete

--- milestone_projects/bitPredict/src/models.py
+++ milestone_projects/bitPredict/src/models.py
@@ -9,7 +9,14 @@ from pprint import pprint
 
 from dataProccessing import *
 
-MODEL_NAMES = ["0_Baseline_Naive", "1_Dense_W7_H1", "2_Dense_W30_H1", "3_Dense_W30_H7"]
+MODEL_NAMES = [
+    "0_Baseline_Naive",
+    "1_Dense_W7_H1",
+    "2_Dense_W30_H1",
+    "3_Dense_W30_H7",
+    "4_Conv1D",
+    "5_LSTM"
+]
 
 def model_0():
     """
@@ -31,10 +38,7 @@ def model_0():
     return model_0_results
 
 def model_1(dir_num, HORIZON=1, WINDOW_SIZE=7):
-    """
-
-    """
-
+    """ Dense model using varying window and horizon sizes """
 
     train_windows, train_labels, test_windows, test_labels = create_train_test_datasets(window=True, horizon=HORIZON, window_size=WINDOW_SIZE)
 
@@ -79,14 +83,121 @@ def model_1(dir_num, HORIZON=1, WINDOW_SIZE=7):
     return
 
 
+def model_4():
+    """ Conv1D Model """
+
+    HORIZON = 1
+    WINDOW_SIZE = 7
+    train_windows, train_labels, test_windows, test_labels = create_train_test_datasets(window=True, horizon=HORIZON, window_size=WINDOW_SIZE)
+
+    # To use Conv1D, need input shape of (batch_size, timesteps, input_dim)
+    x = tf.constant(train_windows[0])
+    # add extra dim for input dim i.e. (7,) -> (7,1)
+    expand_dims_layer = layers.Lambda(lambda x: tf.expand_dims(x, axis=1))
+
+    print("Testing lambda layer\n" + 40*"-")
+    print(f"Original shape: {x.shape}")
+    print(f"Expanded shape: {expand_dims_layer(x).shape}")
+    print(f"Original values with expanded shape:\n {expand_dims_layer(x)}")
+
+    tf.random.set_seed(42)
+
+    model_4 = tf.keras.Sequential([
+       layers.Lambda(lambda x: tf.expand_dims(x, axis=1)),
+       layers.Conv1D(filters=128, kernel_size=5, strides=1, padding="causal", activation="relu"),
+       layers.Dense(HORIZON)
+    ], name=MODEL_NAMES[4])
+
+    model_4.compile(loss="mae", optimizer="adam", metrics=["mae", "mse"])
+
+    model_4.fit(
+        train_windows,
+        train_labels,
+        batch_size=128,
+        epochs=100,
+        verbose=2,
+        validation_data=(test_windows, test_labels),
+        callbacks=[create_model_checkpoint(4, MODEL_NAMES[4])]
+    )
+
+    print(model_4.summary())
+    model_4 = tf.keras.models.load_model(f"../models/4/{MODEL_NAMES[4]}")
+    model_4_preds = make_preds(model_4, test_windows)
+    print(f"\nModel 4 Results:\n")
+    model_1_results = evaluate_preds(tf.squeeze(test_labels), model_4_preds)
+    # plot model
+    _, _, X_test, _ = create_train_test_datasets()
+    offset = 300
+    plt.figure(figsize=(10,7))
+    # index into test_labels to account for the test window offset
+    plot_time_series(timesteps=X_test[-len(test_windows):],  values=test_labels[:, 0], start=offset, label="test data")
+    plot_time_series(timesteps=X_test[-len(test_windows):],  values=model_4_preds, start=offset, format="-", label=f"model 4 preds")
+    plt.savefig(f"../models/4/model_4_predictions.png", dpi=250, bbox_inches="tight")
+
+    return
+
+
+def model_5():
+    """ LSTM Model """
+    HORIZON = 1
+    WINDOW_SIZE = 7
+    train_windows, train_labels, test_windows, test_labels = create_train_test_datasets(window=True, horizon=HORIZON, window_size=WINDOW_SIZE)
+
+    tf.random.set_seed(42)
+
+    inputs = layers.Input(shape=(WINDOW_SIZE))
+    x = layers.Lambda(lambda x: tf.expand_dims(x, axis=1))(inputs)
+    # x = layers.LSTM(128, return_sequences=True)(x)
+    x = layers.LSTM(128, activation="relu")(x)
+    x = layers.Dense(32, activation="relu")(x)
+    outputs = layers.Dense(HORIZON)(x)
+
+    model_5 = tf.keras.Model(inputs, outputs, name=MODEL_NAMES[5])
+
+    model_5.compile(loss="mae", optimizer="adam")
+
+    model_5.fit(
+        train_windows,
+        train_labels,
+        batch_size=128,
+        epochs=100,
+        verbose=2,
+        validation_data=(test_windows, test_labels),
+        callbacks=[create_model_checkpoint(5, MODEL_NAMES[5])]
+    )
+
+    print(model_5.summary())
+    model_5 = tf.keras.models.load_model(f"../models/5/{MODEL_NAMES[5]}")
+    model_5_preds = make_preds(model_5, test_windows)
+    print(f"\nModel 5 Results:\n")
+    model_1_results = evaluate_preds(tf.squeeze(test_labels), model_5_preds)
+    # plot model
+    _, _, X_test, _ = create_train_test_datasets()
+    offset = 300
+    plt.figure(figsize=(10,7))
+    # index into test_labels to account for the test window offset
+    plot_time_series(timesteps=X_test[-len(test_windows):],  values=test_labels[:, 0], start=offset, label="test data")
+    plot_time_series(timesteps=X_test[-len(test_windows):],  values=model_5_preds, start=offset, format="-", label=f"model 5 preds")
+    plt.savefig(f"../models/5/model_5_predictions.png", dpi=250, bbox_inches="tight")
+
+    return
+
+
+def train_and_save_model(model_num):
+    """ Trains, saves and evaluates a model """
+
+    if not os.path.exists(f"../models/{model_num}"):
+        os.mkdir(f"../models/{model_num}")
+        sys.stdout = open(f"../models/{model_num}/{MODEL_NAMES[model_num]}.log", "w")
+        print(f"Model: {MODEL_NAMES[model_num]}\n")
+        eval(f"model_{model_num}()")
+    return
+
+
 def train_all_models():
     """ Trains all models functionally defined in the format model_{INT} """
-    if not os.path.exists(f"../models/0"):
-        os.mkdir(f"../models/0")
 
-        sys.stdout = open(f"../models/0/{MODEL_NAMES[0]}.log", "w")
-        print(f"Model: {MODEL_NAMES[0]}\n")
-        model_0()
+    train_and_save_model(0) 
 
     if not os.path.exists(f"../models/1"):
         os.mkdir(f"../models/1")
@@ -109,6 +220,9 @@ def train_all_models():
         print(f"Model: {MODEL_NAMES[3]}\n")
         model_1(3, HORIZON=7, WINDOW_SIZE=30)
 
+    train_and_save_model(4)  
+    train_and_save_model(5)  
+
     return
 
 def compare_model_performances(model_names=MODEL_NAMES):
@@ -125,7 +239,7 @@ def compare_model_performances(model_names=MODEL_NAMES):
             # and then convert lines -> str -> dict -> pandas df.
             lines = f.readlines()[-5:]
             json_string = json.dumps(ast.literal_eval(u' '.join(lines)))
-            metrics = json.loads(json_string)  # Conert json str to dict
+            metrics = json.loads(json_string)  # Convert json str to dict
             metrics_and_name = {**{"Model": model_name}, **metrics}
 
             df = df.append(metrics_and_name, ignore_index=True)
@@ -138,7 +252,7 @@ def compare_model_performances(model_names=MODEL_NAMES):
     df.plot(x="Model", y="MAE", kind="barh")
     plt.xlabel("Mean absolute error")
     plt.savefig("../models/model_performance_MAE_plot.png", bbox_inches="tight", dpi=250)
-    
+    return
 
 
 if __name__ == "__main__":
